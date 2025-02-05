d5ef81cf5ab1aa72671468875486622b34bd466c
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2025-02-05 12:01:13 +0530

    Add files via upload

---
+++ 3_DataFrame_Basic_Operations/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Read
+df = pd.read_csv('data.csv')
+
+
+# Append new row (Use `pd.concat` instead of `append` in newer pandas versions)
+df = pd.concat([df, pd.DataFrame([{'Name': 'Vijay', 'Age': 59, 'Department': 'vIr'}])], ignore_index=True)
+
+print("After Append:")
+print(df)
+
+# Drop row with index 4 (Use `inplace=True` to modify `df`)
+df.drop(5, inplace=True)
+
+print("After Drop:")
+print(df)
